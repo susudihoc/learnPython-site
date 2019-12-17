fa28ebdd4a0a8715fb5d0879e31e8d49cb7f5a97
susudihoc <buiquangnhat782@gmail.com>
2019-12-17 14:29:10 +0900

    Japanese Web, first

---
+++ JapaneseWeb/polls/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+ 
+def index(request):
+    response = HttpResponse()
+    response.write("<h1>Welcome</h1>")
+    response.write("This is the polls app")
+    return response
